bc8c1b5cf363900bcc3c9dc923a2560e71ffb8be
samehpalas <127115860+samehpalas@users.noreply.github.com>
2024-03-21 23:12:58 +0200

    Update buildIMG.groovy

--- vars/buildIMG.groovy
+++ vars/buildIMG.groovy
@@ -1,6 +1,6 @@
 #! /usr/bin/env groovy
 
-import com.example.Docker
+import com.example.Docker //call functions of scr
 
 def call(String imgName) {
     return new Docker(this).dockerbuild (imgName)
