f8dca9c1a604e1b6939775d3ac494ab14d83e10a
samehpalas <samehpalas33@gmail.com>
2024-03-25 02:20:48 +0200

    adding full cicd

---
+++ target/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+  return '<h1>Hello, World!</h1>'
+
+@app.route('/morning')
+def hello():
+  return 'ahlan w sahlan'
+
+
+
+if __name__ == "__main__":
+  app.run(debug=False,host='0.0.0.0',port=5000)
